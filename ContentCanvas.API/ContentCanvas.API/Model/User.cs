82b971b7fbacd9a7c4f0d94a20d497e98771965b
asafarim <asafarim.it@gmail.com>
2023-12-19 23:35:24 +0100

    Add User.cs

---
+++ ContentCanvas.API/ContentCanvas.API/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ContentCanvas.API.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
