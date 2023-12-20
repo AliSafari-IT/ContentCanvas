af47437151e45a350a2d7f8169ffb3c3dae49614
asafarim <asafarim.it@gmail.com>
2023-12-20 16:48:07 +0100

    Update User.cs

--- ContentCanvas.API/ContentCanvas.API/Model/User.cs
+++ ContentCanvas.API/ContentCanvas.API/Model/User.cs
@@ -1,11 +1,25 @@
-﻿namespace ContentCanvas.API.Model
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ContentCanvas.API.Model
 {
     public class User
     {
-        public int Id { get; set; }
+        // This property represents the MongoDB ObjectId.
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+
+        // Other properties as before
+        [BsonRequired]
         public required string Username { get; set; }
+
+        [BsonRequired]
         public required string Email { get; set; }
+
+        [BsonRequired]
         public required string Password { get; set; }
+
         public string? Role { get; set; }
     }
 }
