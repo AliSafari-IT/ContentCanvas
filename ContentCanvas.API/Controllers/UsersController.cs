6fc2da2a7c92a0c302d9437a0f89cb3fe919472b
asafarim <asafarim.it@gmail.com>
2023-12-28 18:03:55 +0100

    Add UsersController.cs

---
+++ ContentCanvas.API/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using ContentCanvas.API.Data;
+using ContentCanvas.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ContentCanvas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(MongoDbContext context) : Controller
+    {
+        private readonly MongoDbContext _context = context;
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return await _context.Users.Find(_ => true).ToListAsync();
+        }
+
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            var user = await _context.Users.Find(user => user.IdObject == id).FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _context.Users.InsertOneAsync(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.IdObject }, user);
+        }
+
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(string id, User user)
+        {
+            if (id != user.IdObject)
+            {
+                return BadRequest();
+            }
+
+            var result = await _context.Users.ReplaceOneAsync(u => u.IdObject == id, user);
+
+            if (result.MatchedCount == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            var result = await _context.Users.DeleteOneAsync(user => user.IdObject == id);
+
+            if (result.DeletedCount == 0)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
