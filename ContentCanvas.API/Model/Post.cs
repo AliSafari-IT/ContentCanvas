a9d44376486c05bd487c80bb91b9f6ad52cd4694
asafarim <asafarim.it@gmail.com>
2023-12-28 18:03:56 +0100

    Add Post.cs

---
+++ ContentCanvas.API/Model/Post.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace ContentCanvas.API.Model
+{
+    public class Post
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string IdObject { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public bool IsPublic { get; set; }
+
+        public string UserId { get; set; }
+
+        public Post()
+        {
+            IdObject = Guid.NewGuid().ToString();
+        }
+    }
+}
