fb990d83232be547c48d7b6da42bac2c6c5ac19a
asafarim <asafarim.it@gmail.com>
2024-01-01 23:20:57 +0100

    Update App.tsx

--- content-canvas-app/src/App.tsx
+++ content-canvas-app/src/App.tsx
@@ -1,13 +1,15 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import PermissionList from './components/PermissionList';
 import UsersPanel from './components/UsersPanel';
+import { store } from './store';
 
 function App() {
   return (
-    <div className="">
+    <Provider store={store}>
       <UsersPanel />
       <PermissionList />
-    </div>
+    </Provider>
   );
 }
 
