a6d4245b78903f31e87bd24b69358eefef9910a6
asafarim <asafarim.it@gmail.com>
2024-01-01 23:20:57 +0100

    Update UserList.tsx

--- content-canvas-app/src/components/UserList.tsx
+++ content-canvas-app/src/components/UserList.tsx
@@ -1,125 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { IUser } from '../interfaces/iUser';
-import { IUserRole } from '../interfaces/iUserRole';
-import { IRole } from '../interfaces/iRole';
+import React, { useEffect } from 'react';
 import SortableColumnHeader from '../utilityComponents/SortableColumnHeader ';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store';
+import { fetchUsers, sortUsers } from '../slices/userSlice';
 
 const UserList: React.FC = () => {
-    const [users, setUsers] = useState<IUser[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const allResponses = await Promise.allSettled([
-                    fetch('http://localhost:56596/api/Users'),
-                    fetch('http://localhost:56596/api/Roles'),
-                    fetch('http://localhost:56596/api/UserRoles')
-                ]);
-
-                const [usersResponse, rolesResponse, userRolesResponse] = allResponses;
-
-                if (usersResponse.status === 'fulfilled' && rolesResponse.status === 'fulfilled' && userRolesResponse.status === 'fulfilled') {
-                    const usersData = await usersResponse.value.json() as IUser[];
-                    const rolesData = await rolesResponse.value.json() as IRole[];
-                    const userRolesData = await userRolesResponse.value.json() as IUserRole[];
-
-                    const roleIdToName = rolesData.reduce((acc, role) => {
-                        acc[role.idObject] = role.name;
-                        return acc;
-                    }, {} as { [key: string]: string });
-
-                    const updatedUsersData = usersData.map((user, index) => ({
-                        ...user,
-                        index: index + 1,
-                        roleNames: userRolesData
-                            .filter(ur => ur.userIdObject === user.idObject)
-                            .map(ur => roleIdToName[ur.roleIdObject])
-                            .filter(name => name)
-                    }));
-
-                    setUsers(updatedUsersData);
-                } else {
-                    console.log('HTTP error while fetching data');
-                    throw new Error('HTTP error while fetching data');
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
-
+    const dispatch = useDispatch<AppDispatch>();
+    const { users, loading } = useSelector((state: RootState) => state.user);
     const handleSort = (columnIndex: number, direction: 'asc' | 'desc') => {
-        const sortedUsers = [...users].sort((a, b) => {
-            let valueA, valueB;
-
-            // For Roles (assuming it's an array of strings)
-            if (columnIndex === 5) {
-                // Concatenate role names into a single string for comparison
-                const rolesA = a.roleNames?.join(', ') ?? '';
-                const rolesB = b.roleNames?.join(', ') ?? '';
-
-                return direction === 'asc' ? rolesA.localeCompare(rolesB) : rolesB.localeCompare(rolesA);
-            }
-
-            switch (columnIndex) {
-                case 0:  // For Index column
-                valueA = a.index;
-                valueB = b.index;
-                break;
-                case 1: // First Name
-                    valueA = a.firstName;
-                    valueB = b.firstName;
-                    break;
-                case 2: // Last Name
-                    valueA = a.lastName;
-                    valueB = b.lastName;
-                    break;
-                case 3: // Username
-                    valueA = a.username;
-                    valueB = b.username;
-                    break;
-                case 4: // Email
-                    valueA = a.email;
-                    valueB = b.email;
-                    break;
-
-                case 6: // Created On (as date objects for proper comparison)
-                    valueA = new Date(a.createdOn);
-                    valueB = new Date(b.createdOn);
-                    break;
-            }
-
-            // For string comparison
-            if (typeof valueA === 'string' && typeof valueB === 'string') {
-                return direction === 'asc' ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
-            }
-
-            if (typeof valueA === 'number' && typeof valueB === 'number') {
-                return direction === 'asc' ? valueA - valueB : valueB - valueA;
-            }
-            // For date comparison
-            if (columnIndex === 6) { // Assuming 6 is the index for date column
-                const dateA = new Date(a.createdOn);
-                const dateB = new Date(b.createdOn);
-
-                // Check if both values are valid dates
-                if (!isNaN(dateA.getTime()) && !isNaN(dateB.getTime())) {
-                    return direction === 'asc' ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
-                }
-            }
-
-            return 0;
-        });
-
-        setUsers(sortedUsers);
+        dispatch(sortUsers({ columnIndex, direction }));
     };
 
-
+    useEffect(() => {
+        dispatch(fetchUsers());
+        // Dispatch fetchRoles and fetchUserRoles as well
+    }, [dispatch]);
 
     if (loading) return <p>Loading users...</p>;
 
@@ -139,7 +34,7 @@ const UserList: React.FC = () => {
                     </tr>
                 </thead>
                 <tbody className='tbodyLight hoverLightGray'>
-                    {users.map((user, i) => (
+                    {users.map((user) => (
                         <tr key={user.idObject}>
                             <td>{user.index}</td>
                             <td>{user.firstName}</td>
